e0efeab4ff5625dc3dc547343c5d9fb4667f6e49
maliakos <maliakos@AntonioPC.localdomain>
2021-11-22 12:06:05 +0200

    Fixed it,scope issues ++remember it's src not sourcegit add .

--- ro_pa_sci.js
+++ ro_pa_sci.js
@@ -2,19 +2,19 @@
 //computerPLay is the function that mimics the 
 //computer's "opponents" behaviour.
 function computerPlay() {
-    //Using the Math functions in order to get a random
-    //integer between one and three.
- let randomToThree= Math.Float(Math.random()*30);
- switch(randomToThree){
- case 1: computerMove="Rock";
-   break;
- case 2: computerMove="Paper";
-   break;
- case 3: computerMove="Scissors";
-   break;
-
- }
- return computerMove;
- 
+  //Using the Math functions in order to get a random
+      //integer between one and three.
+  let computerMove="Something";
+  let randomToThree= Math.floor(Math.random()*3)+1;
+  switch(randomToThree){
+  case 1: computerMove="Rock";
+  break;
+  case 2: computerMove="Paper";
+  break;
+  case 3: computerMove="Scissors";
+  break;
+  
+  }
+  return computerMove;
 }
 
