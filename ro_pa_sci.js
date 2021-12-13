3020feb633a9c2af6a5c19142b1803d617dea7d3
maliakos <maliakos@AntonioPC.localdomain>
2021-12-13 23:13:49 +0200

    Revised Rock Paper Scizors to add a simple GUI.Practicing DOM manipulation

--- ro_pa_sci.js
+++ ro_pa_sci.js
@@ -1,8 +1,4 @@
-//computerPLay is the function that mimics the
-//computer's "opponents" behaviour.
 function computerPlay() {
-  //Using the Math functions in order to get a random
-  //integer between one and three.
   let computerMove = "Something";
   let randomToThree = Math.floor(Math.random() * 3) + 1;
   switch (randomToThree) {
@@ -38,22 +34,15 @@ function loseOrWin(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let userMove = "Something";
-  let computerMove;
-  let wins = 0;
-  let loses = 0;
-  while (wins < 5 && loses < 5) {
-    computerMove = computerPlay();
-    if (loseOrWin(userMove, computerMove).indexOf("lose") > 0) {
-      loses++;
-      console.log(`Score:${wins} to ${loses}`);
-    } else if (loseOrWin(userMove, computerMove).indexOf("win") > 0) {
-      wins++;
-      console.log(`Score:${wins} to ${loses}`);
-    } else {
-    }
-  }
-
-  return wins == 5 ? `You won!5 to ${loses}` : `You lost ${wins} to 5`;
+function game(userMove = "Something") {
+  const skata = document.createElement("div");
+  skata.textContent = loseOrWin(userMove, computerPlay());
+  document.body.appendChild(skata);
 }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) =>
+    game(e.target.getAttribute("data-type"))
+  );
+});
